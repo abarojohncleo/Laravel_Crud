2b1e7003c4b660c0a51e42ca4a3bdaa98118b2fa
abarojohncleo <abarojohncleo@gmail.com>
2022-02-17 18:52:55 +0800

    Initial commit CRUD

---
+++ resources/views/products/layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel Crud Sample</title>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+    </head>
+    
+    <body>
+        <div class="container">
+            @yield('content')
+        </div>
+        <!-- <div id="app">
+            <example-component></example-component>
+        </div>
+        <script src="{{ mix('/js/app.js') }}"></script> -->
+    </body>
+</html>
